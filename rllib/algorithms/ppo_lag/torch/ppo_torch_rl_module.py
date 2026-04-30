dc0a06143e942b28b008aece805f4f897e282332
LucasQCosta <lucas.cq.10@gmail.com>
2026-04-30 16:37:44 -0300

    update the path to ppo_lag in ppo_torch_rl_module.py

--- rllib/algorithms/ppo_lag/torch/ppo_torch_rl_module.py
+++ rllib/algorithms/ppo_lag/torch/ppo_torch_rl_module.py
@@ -1,13 +1,12 @@
-# Backward compat import.
-from ray.rllib.algorithms.ppo.torch.default_ppo_torch_rl_module import (  # noqa
+from ray._common.deprecation import deprecation_warning
+from ray.rllib.algorithms.ppo_lag.torch.default_ppo_torch_rl_module import (  # noqa
     DefaultPPOTorchRLModule as PPOTorchRLModule,
 )
-from ray._common.deprecation import deprecation_warning
 
 
 deprecation_warning(
-    old="ray.rllib.algorithms.ppo.torch.ppo_torch_rl_module.PPOTorchRLModule",
-    new="ray.rllib.algorithms.ppo.torch.default_ppo_torch_rl_module."
+    old="ray.rllib.algorithms.ppo_lag.torch.ppo_torch_rl_module.PPOTorchRLModule",
+    new="ray.rllib.algorithms.ppo_lag.torch.default_ppo_torch_rl_module."
     "DefaultPPOTorchRLModule",
     error=False,
 )
